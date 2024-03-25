77d3046217be8591505b8f2225d7675904ccf0c9
JanzaibYousaf <154719041+janzaibyousaf@users.noreply.github.com>
2024-03-25 16:48:53 +0500

    Add files via upload

---
+++ Task_Sir_Basit/16-03-2024/app.js
@@ -0,0 +1,821 @@
+// chapter 1
+
+// task : 1
+
+// alert("Firstname");
+// alert("Lastname");
+// alert("Email");
+// alert("PhoneNumber");
+// alert("Password");
+// alert("You're learning JavaScript!"); 
+
+// chapter 2
+
+// task : 2
+
+// var myName;
+
+// Question 2
+
+// my_varible = "Hello,Sir"
+
+// Question 3
+
+// var teamName;
+
+// teamName = "smit";
+
+// alert(teamName);
+
+// Question 4 
+
+// var fullname = "janzaib";
+
+// var fullname = "yousaf";
+
+// alert(fullname)
+
+// chapter 3
+
+// task 3
+
+// Question 1
+
+// var caseQty;
+
+// Question 2
+
+// var caseQty = 144;
+
+// Question 3 
+
+// var num = 9;
+
+    // Question 4
+
+    // var total = 5 + 7;
+
+    // alert(total);
+
+    // Question 5 
+
+// var merchtotal = 100;
+
+// var shippingcharge = 10;
+
+// ordertotal = merchtotal + shippingcharge
+
+// alert(ordertotal)
+
+    // Question 6
+ 
+// var num = 5;
+
+// var num1 = 3;
+
+// alert(num + num1);
+
+// chapter 4
+
+// Task 4 
+
+// Question 1 
+
+// var productCost = 3.45;
+
+// Question 2 
+
+//  var  nameOfBand;
+
+// Question 3
+
+// var num =  10;
+
+// Question 4 
+
+// var firstName = "JanzaibYousaf"
+
+// // Question 5
+
+// // legal variable
+
+// 1- myName
+// 2- myAge
+// 3- totalPrice
+// 4- first_name
+// 5, _last
+
+// illegal variable
+
+// 123var
+// user-age
+// $price
+// class
+// for
+
+// chapter 5 
+
+// task 5
+
+// Question 1
+
+// Name - Modulo Operator
+// symbol - %
+
+// Question 2 
+
+// Answer is = 2
+
+// Question 3
+
+// largeNum = 1000 * 2000
+
+// Question 4 
+
+// var num = 10 ;
+// var num1 = 6;
+
+// result = num - num1
+
+// console.log(result);
+
+// Question 5 
+
+// remainder = 10 % 4
+// alert(remainder);
+// // Answer 2
+
+// Question 6 
+
+// var num1 = 10; 
+// var num2 = 30;
+// var results = num1 * num2;
+
+// alert(results);
+
+// Chapter 6 
+
+// Task 6 
+
+// Question 1 
+
+// x++;
+
+// x += 1;
+
+// Question 2
+
+// x = x-1;
+
+// Question 3 
+
+// var x = 50;
+// var y = x++;
+
+// Answer 50.
+
+// Question 4 
+
+// var y = 50;
+// var z = --y;
+
+// Answer 49.
+
+// Question 5
+
+// var num = 10;
+// var newNum = --num;
+
+// Question 6
+
+// var num = 10;
+// var num2 = num + 15;
+
+// Question 7
+
+// var num = 5;
+// num++;
+
+// alert(num++)
+
+// Chapter 7 
+
+// Task 7
+
+// Question 1
+
+// var calculatedNum = 2 + (2 * 6);
+
+// Answer 14.
+
+// Question 2 
+
+// var calculatedNum = (2+2) * 6 
+
+// Answer 24.
+
+// Question 3
+
+// var calculatedNum = (2+2)*(4+2);
+
+// Answer 24.
+
+// Question 4
+
+// var calculatedNum = ((2+2) * 4) + 2;
+
+// Answer 18.
+
+// Question 5
+
+// var totalCost = (2 + 2 )* (4 + 10);
+
+// Answer 56.
+
+// Question 6
+
+// var totalCost = 2 + (2 * 4) + 10 ;
+
+// Result 20.
+
+// Question 7
+
+// var pressure = (4 / 2) * 4;
+
+// Answer 5.
+
+// Chapter 8
+
+// Task 8
+
+// Question 1
+
+// var num = "2" + "2";
+
+// Result "22".
+
+// Question 2
+
+// message = ("hello," + "dolly")
+
+// alert(message);
+
+// Question 3
+
+// alert("33" + 3);
+
+// answer 333.
+ 
+// Question 4
+
+// alert("pakistan" + "zindabad");
+
+// Question 5
+
+// var result = "cost" + 10;
+
+// Question 6 
+
+//  var string1 = "hello";
+// var  string2 = "world";
+
+// var concatenated = string1 +" " + string2
+
+// alert(concatenated);
+
+// // chapter 9 
+
+// Task 9
+
+// Question 1
+
+// var first_name = prompt ("Enter Your Firstname")
+
+// alert(first_name);
+
+// Question 2
+
+// var country = prompt ("country","china");
+
+// alert(country);
+
+
+// Question 3
+
+
+// var yourName = prompt("Enter Your name");
+
+
+// Question 4
+
+// var a = prompt ("Message" , "hello");
+// console.log(a)
+
+
+
+// Question 5
+
+// var first = ("Enter education");
+// var second = ("Matric");
+
+// var third = prompt(first ,second );
+
+// console.log(third)
+
+
+// Question 6
+
+
+// var message = ("Enter education");
+// var message1 = ("Matric");
+
+// var third = prompt(message ,message1 );
+
+// console.log(third)
+
+// chapter 10 
+
+// Task 10 
+
+// Question 1
+
+// var city = "Karachi"; 
+// if (city === "Karachi") { 
+// //     console.log("The City of Lights"); 
+// }
+// Question 2
+
+// var x = "z"
+
+// if(x === y){ 
+    
+//     console.log("The value of z is: " + z);
+// }
+
+// Question 3
+
+// var zipcode = 10010
+
+// if(zipcode === 10010){
+
+// alert("karachi");
+
+// }else {
+//     alert("Plz write correct city");
+// }
+
+// Question 4
+
+// var x = 1; 
+
+// if (x === 1) { 
+//     x = 10;
+//     console.log(x);
+// }
+
+// Chapter 11 
+
+// task 11
+
+// Question 1
+
+// if (variable1 !== variable2) {
+    
+// }
+
+// Question 2
+
+// if (variable1 >= variable2) {
+
+// }
+
+// Question 3
+
+// var num = 5
+
+// if(num !== 10) {
+// num = 10 
+// }
+// console.log(num)
+
+// Question 4
+
+// var num1 = 10;
+// var num2 = 20;
+
+// if(num1 !== num2){
+//     alert("congratulation");
+// }
+
+// Question 5
+
+// var firstname = prompt("Enter Your Firstname");
+
+// if(firstname !== "janzaib"){
+//     alert("No Match")
+// }
+
+// Chapter 12
+
+// Task 12
+
+// Question 1 
+
+// var num1 = 10; 
+// var num2 = 5;
+
+// if(num1 >= num2){
+//     alert("ok" );
+// }else{
+//     alert("bye");
+// }
+
+// Question 2
+
+// var a = +prompt ("Enter Your Physics Marks ");
+// var b = +prompt ("Enter Your Math Marks");
+// var c = +prompt ("Enter Your English Marks");
+// var d = +prompt ("Enter Your Chemistry Marks");
+// var e = +prompt ("Enter Your Computer Marks");
+// console.log(a,b,c,d,e)
+// var  totalmarks = a + b + c + d + e;
+
+// var percentage = (totalmarks / 500) * 100;
+// console.log("Percentage: " + percentage.toFixed(2) + "%");
+
+
+// if(percentage >= 90){
+
+// console.log("Grade A")
+
+// }
+// else if(percentage >= 80){
+
+// console.log("Grade B")
+
+// }else if(percentage >= 70){
+
+// console.log("Grade C")
+
+// }else if(percentage >= 60 ){
+
+//     console.log("Grade D")
+
+// }else if(percentage >= 50 ){
+
+//     console.log("Grade E")
+// }else  {
+//     console.log("Grade f")
+// }
+
+// Question 3
+
+// var a = 10
+
+// if(a === 10 ){
+//     alert("A is 10")
+// }else {
+//     alert("A is correct value of 10")
+// }
+
+// Question 4
+
+// var city = prompt("Enter Your City");
+
+// if(city === "karachi"){
+
+//     alert("karachi the city of lights")
+
+// }else if (city === "lahore" ){
+
+// alert("lahore is best")
+
+// }else {
+//     alert("NOT MATCH")
+// }
+
+// Chapter 13
+
+// Task 13
+
+// Question 1
+
+// if(a === b && C === d){
+
+// }
+
+// Question 2
+
+// if (a === b || c !== d){
+
+// }
+
+// Question 3
+
+// if ((name === "hamza " ||  name === "arsalan") && age <60){
+
+// }
+// Question 4
+
+// var num1 = 10;
+// var num2 = 20;
+
+// if(num1 < num2 || num1 > num2 ){
+
+// alert("The first variable is either less than or greater than the second variable")
+// }
+
+
+// Question 5
+
+
+
+// var firstName = "janzaib";
+// var lastName = "yousuf";
+
+
+// var userFirstName = prompt("Please enter your first name");
+
+
+// var userLastName = prompt("Please enter your last name");
+
+
+// if (userFirstName === firstName && userLastName === lastName) {
+//     alert("Your name matches!");
+// } else {
+//     alert("Your name does not match");
+// }
+
+// Chapter 14
+
+// Task 14
+
+// Question 1
+
+
+// var password = prompt("Please enter your password");
+
+
+// if (password !== "5") {
+    
+//     if (password.length <= 5) {
+//         alert("Password must be greater than 5");
+//     } else {
+//         alert("OK");
+//     }
+// } else {
+//     alert("Password cannot be empty");
+// }
+
+// Question 2
+
+// let a = 1;
+// let c = "Max";
+
+// if (a === 1) { 
+//     if (c === "Max") { 
+//         alert("OK"); 
+//     } 
+// }
+
+
+
+// Question 3
+
+// let num1 = 1;
+// let num2 = "Max";
+
+// if (num1 === 1 && num2 === "Max") { 
+//     alert("OK"); 
+// }
+ 
+// Question 4
+
+// let variable1 = 10;
+// let variable2 = 10;
+
+
+// if (variable1 === variable2) {
+
+//     if (variable1 <= variable2) {
+
+//         alert("Both conditions are true!");
+//     }
+// }
+
+// Chapter 15
+
+// Task 15
+
+// Question 1
+
+// let emptyArray = [];
+
+// Question 2
+
+// let stringArray = ["Hello, World!"];
+
+// Question 3
+
+// var alphabet = ["h", "i", "j", "k"];
+// alert(alphabet[2]); 
+
+// Question 4
+
+// var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+// var lengthOfArray = alphabet.length;
+
+// alert(lengthOfArray);
+
+
+// Question 5
+
+// let myArray = ["firstElement"];
+
+// myArray[1] = "secondElement";
+
+// alert(myArray[1]);
+
+// Chapter 16
+
+// Task 16
+
+// Question 1
+
+// let myArray = ["firstElement"];
+
+
+// myArray.push("secondElement");
+
+
+// alert(myArray[myArray.length - 1]); 
+
+// Question 2
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+// sizes.unshift(36, 38, 40);
+
+// console.log(sizes);
+
+
+// Question 3
+
+// let myArray = ["firstElement"];
+
+
+// myArray.unshift("newFirstElement");
+
+
+// alert(myArray[0]); 
+
+// Question 4
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+
+// sizes.splice(2, 0, "L");
+
+// console.log(sizes);
+
+// Question 5
+
+// var sizes = ["S", "M", "XL", "XXL", "XXXL"];
+
+
+// var regSizes = sizes.slice(0, 3);
+
+// console.log(regSizes);
+
+
+// Question 6
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"];
+
+// pets.splice(1, 3); 
+// pets.splice(1, 0, "parrot", "rabbit");  
+
+// console.log(pets); 
+
+// Question 7
+
+// var pets = ["dog", "cat", "ox", "duck", "frog"]; 
+// Remove "cat" and "ox". 
+
+// Question 8
+
+// var pets = ["dog", "cat", "ox", "duck", "frog", "flea"];
+
+
+// var reducedPets = pets.slice(3, 5);
+
+// console.log(reducedPets);
+
+// Chapter 17 - 20
+
+// Task 17 - 20
+
+// Question 1
+
+// for (var i = 0; i < 10; i++) {
+//     console.log("This is iteration number: " + (i + 1));
+// }
+
+// Question 2
+
+// for (var i = 0; i <= 11; i++) {
+
+// }
+
+// Question 3
+
+// ; { }
+
+
+// for (var i = 0; i <= 4; i++) {
+// }
+
+
+// Question 4
+
+// for (var counter = 0; counter < 100; counter++) {
+
+// }
+
+// Question 5
+
+// for (var counter = 0; counter < 100; counter++) {
+    
+// }
+
+// Question 6
+
+// var arrayLength = array.length;
+
+// Question 7
+
+// var flag = true;
+
+
+// Question 8
+
+// for (var i = 0; i < pets.length; i++) {
+    
+// }
+
+// Question 9
+
+// for (var i = 0; i < 10; i++) {
+//     if (i === 1) {
+//         alert("The counter is: " + i);
+//         break;
+//     }
+// }
+
+// Question 10
+
+// for (var i = 0; i < userNames.length; i++) {
+  
+//     if (firstName === userNames[i]) {
+
+//         alert("Welcome, " + firstName + "!");
+
+//         break;
+
+//     } else if (i === userNames.length - 1) {
+
+//         alert("Please write correct user name");
+//     }
+// }
+// Question 11
+
+// var matchFound = false; 
+// var list = ["item1", "item2", "item3"]; 
+// var userInput = prompt("Enter an item"); 
+
+// for (var i = 0; i < list.length; i++) { 
+//     if (userInput === list[i]) { 
+//         alert("Match found"); 
+//         matchFound = true; 
+//         break; 
+//     } 
+// }
+
+// if (!matchFound) {
+//     alert("No match found");
+
+// Question 12
+
+// var firstArr = ["a", "b", "c", "d", "e", "f"];
+// var secondArr = [1, 2, 3, 4, 5, 6];
+
+
+
+//     for (var j = 0; j < secondArr.length; j++) {
+       
+//         console.log(firstArr[j] + secondArr[j]);
+//     }
+
+
+
+
